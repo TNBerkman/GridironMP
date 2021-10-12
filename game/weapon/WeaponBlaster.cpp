6a65616cbbe20631e5fefb2f06db8b7aad7ba436
Tyler Berkman <tb279@njit.edu>
2021-10-12 10:56:43 -0400

    changed blaster to prove i know what im doing

--- game/weapon/WeaponBlaster.cpp
+++ game/weapon/WeaponBlaster.cpp
@@ -427,7 +427,7 @@ stateResult_t rvWeaponBlaster::State_Fire ( const stateParms_t& parms ) {
 
 	
 			if ( gameLocal.time - fireHeldTime > chargeTime ) {	
-				Attack ( true, 1, spread, 0, 1.0f );
+				Attack ( true, 100, spread, 0, 1.0f ); //MOD: num_attacks was 1
 				PlayEffect ( "fx_chargedflash", barrelJointView, false );
 				PlayAnim( ANIMCHANNEL_ALL, "chargedfire", parms.blendFrames );
 			} else {
